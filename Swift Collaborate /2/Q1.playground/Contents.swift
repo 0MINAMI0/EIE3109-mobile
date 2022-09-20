c9f892b6af227101f2bddcc467fa1e85976e1626
0MINAMI0 <haolyu1125@gmail.com>
2022-09-20 12:34:21 +0800

    collaborate Q2

--- Swift Collaborate /2/Q1.playground/Contents.swift
+++ Swift Collaborate /2/Q1.playground/Contents.swift
@@ -1,3 +1,14 @@
-import Cocoa
+let set1: Set = ["a", "b", "c", "d"]
+let set2: Set = [ "q", "c", "d"]
+let set3: Set = ["c","w"]
+let set4: Set = ["a", "b", "a", "d"]
 
-var greeting = "Hello, playground"
+print(set1 == set2)
+print((set1.union(set2)).count)
+print(set2.intersection(set3))
+print((set2.subtracting(set3)).count)
+print((set2.symmetricDifference(set3)).count)
+print(set4.isSubset(of: set1))
+print(set2.isSuperset(of: set3))
+print(set3.isDisjoint(with: set4))
+
